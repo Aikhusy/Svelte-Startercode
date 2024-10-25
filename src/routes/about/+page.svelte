d7b3c88e1fd0bb75d26ee12b37cb7a96d40df18a
Syaikhul Shafwan R <heymepoo@gmail.com>
2024-10-25 10:06:13 +0700

    feat : component svelte mirip next js

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<h1>abouts</h1>
+
+<script module>
+	let total = 0;
+</script>
+
+<script>
+	total += 1;
+	console.log(`instantiated ${total} times`);
+</script>
+
+<style>
+	p {
+		/* this will only affect <p> elements in this component */
+		color: burlywood;
+	}
+</style>
